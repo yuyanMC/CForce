6196e91405531852785056ca241f614b8d3af3a7
yuyanMC <yuyanMC@users.noreply.github.com>
2023-07-24 13:45:29 +0000

    deploy: 47693c17bcfb3e0b00c98de729c988ed09714e92

---
+++ assets/timer-62c64910.js
@@ -0,0 +1,1 @@
+const t=[],n=[],o=!1,e=3600,s="./timer",c={notes:t,animationNotes:n,bgsound:!1,length:3600,script:s};export{n as animationNotes,o as bgsound,c as default,e as length,t as notes,s as script};
